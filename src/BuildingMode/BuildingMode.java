40b4d16d990b4554a0899aad7866290b3fd3c92a
Ege <Ege@172.20.132.178>
2023-04-13 17:11:00 +0300

    modified map package, problem in Bridge class (can't make territory[] array?)

---
+++ src/BuildingMode/BuildingMode.java
@@ -0,0 +1,10 @@
+package BuildingMode;
+
+public class BuildingMode<Handler> {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/map/Bridge.java
@@ -0,0 +1,23 @@
+package map;
+
+public class Bridge {
+
+	Territory t1;
+	Territory t2;
+	
+	private void createBridge(Territory ter1, Territory ter2) {
+		this.t1 = ter1;
+		this.t2 = ter2;
+	}
+	
+	public Territory[] getTerritories() {
+
+		Territory[] terList = Territory[2];
+		
+		Territory[0] = t1;
+		Territory[1] = t2;
+		
+		return terList;
+		
+	}
+}
