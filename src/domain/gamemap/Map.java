1863355fd3d4e3a3a88f625ab87b5ae148c4b4e7
yigitfatihcakilli <yigitfatihcakilli@localhost>
2023-05-02 23:43:34 +0300

    updated ArmyPiece, Map and Territory methods. Singleton implementation for Map and Territory classes

--- src/domain/gamemap/Map.java
+++ src/domain/gamemap/Map.java
@@ -1,6 +1,11 @@
 package domain.gamemap;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import domain.Dice;
+import domain.Player;
 import domain.buildingmode.*;
 import domain.gamemap.*;
 
@@ -9,7 +14,8 @@ public class Map {
 	
 	private static int MAXCONTINENT = 6;
 	public static ArrayList<Continent> continents =  new ArrayList<Continent>(); //arraylist
-	
+	private static HashSet<Territory> adjacencySet = new HashSet<Territory>();
+
 	
 	public static Continent continent1;
 	public static Continent continent2;
@@ -17,19 +23,26 @@ public class Map {
 	public static Continent continent4;
 	public static Continent continent5;
 	public static Continent continent6;
+	private static Map single_map_instance=null;
 	
 	/*private void initContinent(Continent c) {
 		
 		continentCount++;
 	}*/
 	
-	public Map() {
+	private Map() {
 		continent1 = Continent.CONTINENT1;
 		continent2 = Continent.CONTINENT2;
 		continent3 = Continent.CONTINENT3;
 		continent4 = Continent.CONTINENT4;
 		continent5 = Continent.CONTINENT5;
 		continent6 = Continent.CONTINENT6;
+	}
+	public static Map Map_initiation() {
+		if (single_map_instance == null) {
+			single_map_instance = new Map();
+		}
+		return single_map_instance;
 	}
 	
 	public void specifyContinent(Continent c) {
@@ -52,6 +65,37 @@ public class Map {
 	}
 	
 	private void checkMap() {
+		
+	}
+	public boolean checkTerritoryAttackValidity(Territory attackTerritory,Territory targetTerritory) {
+		if(isNeighbouring(attackTerritory, targetTerritory)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+
+
+	}
+	private boolean isNeighbouring(Territory territory, Territory territoryQuiry) {
+		HashSet<Territory> territoryAdjancencySet= territory.getAdjacencySet();
+		if(territoryAdjancencySet.contains(territoryQuiry)) {
+			return true;}
+		else {
+			return false;
+		}
+	}
+	public void updateMapBattle(Territory attackerTerritory, Territory defenderTerritory, Player winner) {
+		if(attackerTerritory.getTerritoryOwner().equals(winner)) {
+			defenderTerritory.updateTerritory(-1, "infantry");
+			HashMap<String, Integer> territoryArmyNumbers= defenderTerritory.getTerritoryArmyNumber();
+			if(territoryArmyNumbers.get("infantry")==0) {
+				
+			}
+		}
+		else {
+			attackerTerritory.updateTerritory(-2, "infantry");
+		}		
 		
 	}
 
