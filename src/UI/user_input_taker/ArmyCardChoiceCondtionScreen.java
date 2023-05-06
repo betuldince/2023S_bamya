05e20039608cb294115393100dda111d84d26887
MelihSahinEdu <melihsahin21@ku.edu.tr>
2023-05-06 21:37:15 +0300

    Army Card Choice Taker GUI

--- src/UI/user_input_taker/ArmyCardChoiceCondtionScreen.java
+++ src/UI/user_input_taker/ArmyCardChoiceCondtionScreen.java
@@ -1,10 +1,41 @@
 package UI.user_input_taker;
 
+import javax.swing.JOptionPane;
+
 public class ArmyCardChoiceCondtionScreen {
 	public ArmyCardChoiceCondtionScreen() {
 		
 	}
 	public static int run(boolean c1,boolean c2,boolean c3,boolean c4,boolean c5) {
+		String input="";
+		if (c1) {
+			input+=" 1";
+			
+		}
+		if (c2) {
+			input+=" 2";
+			
+		}
+		if (c3) {
+			input+=" 3";
+			
+		}
+		if (c4) {
+			input+=" 4";
+			
+		}
+		if (c5) {
+			input+=" 5";
+			
+		}
+		
+		
+
+		int ret=Integer.parseInt(JOptionPane.showInputDialog(null,"Enter which card trading you would like to proceed with :"+input));
+		return ret;
+		
+				
+				
 		
 	}
 
