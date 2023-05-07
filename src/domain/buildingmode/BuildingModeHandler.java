afcd6f2017fbe762b7a2cff8699cdd3b57683c2e
Ege <Ege@172.20.57.124>
2023-05-07 14:59:15 +0300

    Huge changes
    Map -> GameMap
    Territory class -> Territory enum
    UI methods that add checkboxes or panels automatically

--- src/domain/buildingmode/BuildingModeHandler.java
+++ src/domain/buildingmode/BuildingModeHandler.java
@@ -4,41 +4,51 @@ import UI.gamemap.*;
 import domain.gamemap.*;
 import domain.initArmyTerritory.*;
 
-public class BuildingModeHandler {
 
-	public static Map map;
+public class BuildingModeHandler implements GameMapListener {
+
+	public static GameMap gameMap;
 	public int playerCount;
 	public int compPlayerCount;
 	
 	//singleton pattern
 	private static BuildingModeHandler buildHandle=null;
-	private BuildingModeHandler(Map map, int playerCount, int compPlayerCount) {
-		this.map = map;
+	private BuildingModeHandler(GameMap gameMap) {
+		this.gameMap = gameMap;
 		this.playerCount = playerCount;
 		this.compPlayerCount = compPlayerCount;
 	}
-	public static BuildingModeHandler createBuildHandler(Map map, int playerCount, int compPlayerCount) {
+	public static BuildingModeHandler createBuildHandler(GameMap gameMap) {
 		if (buildHandle == null) {
-			buildHandle = new BuildingModeHandler(map, playerCount, compPlayerCount);
+			buildHandle = new BuildingModeHandler(gameMap);
 		}
 		return buildHandle;
 	}
 	
-	
-	
-	private void specifyPlayerNum(int p) {
-		this.playerCount = p;
-	}
-
-	private void specifyCompPlayerNum(int cp) {
-		this.compPlayerCount = cp;
-	}
-	
 	public void specifyContinent(Continent c) {
-		map.specifyContinent(c); 
+		gameMap.specifyContinent(c); 
 	}
 	
-	private void specifyTerritory(Territory t) {
+	public void specifyTerritory(Continent c, Territory t) {
+		gameMap.specifyTerritory(c, t);
+	}
+	
+	public void initTerritoryButtonPressed() {
+		
+	}
+	
+	public void selectTerritories() {
+		// TODO Auto-generated method stub
+		WorldMap.frame.dispose();
+		System.out.println("selectedContinents() works");
+		WorldMap.InitiateTerritoriesUI(gameMap, buildHandle);
+	}
+	@Override
+	public void nextPhase() {
+		// TODO Auto-generated method stub
+		WorldMap.frame.dispose();
+		System.out.println("nextPhase() works");
+		
 		
 	}
 	
