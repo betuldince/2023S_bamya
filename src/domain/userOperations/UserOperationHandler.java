53bcb91530fcd4b5e33cb76e8515c45187911621
abakcaci20 <99614005+abakcaci20@users.noreply.github.com>
2023-06-04 15:19:19 +0300

    Created specifications of requestLogin method in UserOperationHandler

--- src/domain/userOperations/UserOperationHandler.java
+++ src/domain/userOperations/UserOperationHandler.java
@@ -6,7 +6,30 @@ public class UserOperationHandler {
 
 	public UserOperationHandler() {
 	}
-	
+
+	/**
+	 * requestLogin:
+	 * Requests login to the game with the given player information.
+	 * <p>
+	 * REQUIRES: A user database exists.
+	 * MODIFIES: Adds the login information to the singleton CurrentLogins.
+	 * EFFECTS: Returns the result (which is type ValidationEnum) of the login request. 
+	 * 			This means the following:
+	 * 			If nickname OR password contains an invalid character or their lengths are not in valid range:
+	 * 				return INVALID_INPUT
+	 * 			Else if nickname doesn't exist in the user database:
+	 * 				return NICKNAME_NOT_FOUND
+	 * 			Else if nickname exists yet password doesn't match with nickname:
+	 * 				return INCORRECT_PASSWORD
+	 * 			Else if nickname is in CurrentLogins:
+	 * 				return ALREADY_LOGGED_IN
+	 * 			Else:
+	 * 				return VALID_LOGIN
+	 * 
+	 * @param nickname a string representing the user nickname
+	 * @param password a string representing the user's password 
+	 * @return result of login request: ValidationEnum
+	 */
 	public ValidationEnum requestLogin(String nickname, String password) {
 		Validation login = new Login(nickname, password);
 		ValidationEnum result = login.getResult();
